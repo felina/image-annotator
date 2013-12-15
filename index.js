6be65ae27b524dcb4eb2547550b22fe5e0c8ab52
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2013-12-15 04:08:59 +0000

    removed dependency on style.css

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
+
 (function( $ ) {
-  $.fn.annotator = function(src) {
+  $.fn.annotator = function(src, width, height) {
     return this.each(function() {
+      $parent = $(this);
       var $zoomin = $('<button>Zoom In</button>');
       var $zoomout = $('<button>Zoom Out</button>');
-      var $img = $('<img class="iggy" src="'+src+'"></img>');
-      var $container = $('<div class="acontainer"></div>');
-      $(this).append($zoomin);
-      $(this).append('<br></br>');
-      $(this).append($zoomout);
-      $(this).append($container);
+      var $img = $('<img src="'+src+'"></img>');
+      var $container = $('<div></div>');
+      $parent.append($zoomin);
+      $parent.append('<br></br>');
+      $parent.append($zoomout);
+      $parent.append($container);
+      $container.css({
+        "width" : width+"px",
+        "height" : height+"px",
+        "position" : "relative",
+        "margin" : "20px 0 0 0",
+        "background" : "gray",
+        "overflow" : "hidden"
+      });
       $container.append($img);
       $img.draggable();
+      $img.css({
+        "position" : "absolute",
+        "left" : "0",
+        "top" : "0",
+        "z-index" : "1",
+        "height" : "100%",
+        "width" : "100%"
+      });
       var originalHeight = $img.height();
       $zoomin.on("click", function (){
-        console.log("zoomin"); zoom($img, originalHeight, 1.25);
+        zoom($img, $parent, originalHeight, 1.25);
       });
       $zoomout.on("click", function(){
-        console.log("zoomout"); zoom($img, originalHeight, 0.8);
+        zoom($img, $parent, originalHeight, 0.8);
       });
     });
   };
 }( jQuery ));
 
-var zoom = function($img, originalHeight, adjustment){
+var zoom = function($img, $parent, originalHeight, scale){
   var offset = $img.offset();
   var width = $img.width();
   var height = $img.height();
   
-  if(adjustment < 1 && originalHeight >= height)
+  if(scale < 1 && originalHeight >= height)
       return;
-  
-  var newWidth = width * adjustment;
-  var newHeight = height * adjustment;
-  
-  var diffWidth = newWidth - width;
-  var diffHeight = newHeight - height;
-  
-  var hcenter = $('body').width() / 2;
-  var vcenter = $('body').height() / 2;
-  
-  var leftPercent = (hcenter - offset.left) / width;
-  var topPercent = (vcenter - offset.top) / height;
+
+  var diffWidth = (width * scale) - width;
+  var diffHeight = (height * scale) - height;
+
+  var leftPercent = (($parent.width() / 2) - offset.left) / width;
+  var topPercent = (($parent.height() / 2) - offset.top) / height;
 
   $img.offset({top: offset.top - (diffHeight * topPercent), left: offset.left - (diffWidth * leftPercent)});
 
-  $img.width(newWidth).height(newHeight);
+  $img.width(width * scale).height(height * scale);
 };
-
-
-
-
-  // function insertImage(a,src){
-  //   $('#'+a).append('<a id="zoomin" href="#">Zoom In</a>');
-  //   $('#'+a).append('<a id="zoomout" href="#">Zoom Out</a>');
-  //   $('#'+a).append('<div id="container"><img id="iggy" src="'+src+'"></img></div>');
-  // }
-  // $("img").draggable();
-  // var $container = $("#container");
-  // var $img = $("#iggy");
-  // console.log(originalHeight);
-// $(document).ready(function(){
-//   var $image;
-//   $('#insert').on("click", function(){
-//     insertImage("img","http://www.emperor-penguin.com/penguin-chick.jpg");
-//     $("img").draggable();
-//     $img = $("#iggy");
-//   });
-//   $("#zoomin").on("click", function (){console.log("zoomin"); zoom(1.25)});
-//   $("#zoomout").on("click", function(){console.log("zoomin"); zoom(0.8)});
-
-// });
