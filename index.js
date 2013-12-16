47b9ed3e1573675dade909aaaabc1608a26c7d96
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2013-12-16 14:46:21 +0000

    canvas and drawing logic

--- index.js
+++ index.js
@@ -1,46 +1,106 @@
+var imgcss = {
+  position : "absolute",
+  left : 0,
+  top : 0,
+  "z-index" : 1,
+};
+
+var containercss = {
+  position : "relative",
+  // "margin" : "20px 0 0 0",
+  // "background" : "gray",
+  overflow : "hidden"
+};
+
+var canvascss = {
+  position : "absolute",
+  left : 0,
+  top : 0,
+  "z-index" : 100,
+};
+
+var list = '';
 
 (function( $ ) {
   $.fn.annotator = function(src, width, height) {
     return this.each(function() {
       $parent = $(this);
-      var $zoomin = $('<button>Zoom In</button>');
-      var $zoomout = $('<button>Zoom Out</button>');
-      var $img = $('<img src="'+src+'"></img>');
-      var $container = $('<div></div>');
-      $parent.append($zoomin);
-      $parent.append('<br></br>');
-      $parent.append($zoomout);
-      $parent.append($container);
-      $container.css({
-        "width" : width+"px",
-        "height" : height+"px",
-        "position" : "relative",
-        "margin" : "20px 0 0 0",
-        "background" : "gray",
-        "overflow" : "hidden"
-      });
-      $container.append($img);
-      $img.draggable();
-      $img.css({
-        "position" : "absolute",
-        "left" : "0",
-        "top" : "0",
-        "z-index" : "1",
-        "height" : "100%",
-        "width" : "100%"
-      });
+      
+      var $zoomin = $('<button>Zoom In</button>').appendTo($parent);
+
+      var $zoomout = $('<button>Zoom Out</button>').appendTo($parent);
+
+      var $container = $('<div></div>')
+                        .css(containercss)
+                        .width(width)
+                        .height(width).appendTo($parent);
+
+      var $img = $('<img src="'+src+'"></img>')
+                    .draggable()
+                    .css(imgcss)
+                    .width(width)
+                    .height(height)
+                    .appendTo($container);
+
+      var $canvas = $('<canvas></canvas>')
+                      .css(canvascss)
+                      .appendTo($container);
+
       var originalHeight = $img.height();
-      $zoomin.on("click", function (){
-        zoom($img, $parent, originalHeight, 1.25);
+
+      if(typeof G_vmlCanvasManager != 'undefined') {
+        console.log("badamdisssss");
+  canvas = G_vmlCanvasManager.initElement(canvas);
+}
+
+      $zoomin.on("click", function (){ zoom($img, $container, originalHeight, 1.25); });
+      $zoomout.on("click", function(){ zoom($img, $container, originalHeight, 0.8); });
+      var context = $canvas[0].getContext("2d");
+      $canvas[0].width = width;
+      $canvas[0].height = height;
+      //$img[0].width = width;
+      //$img[0].height = height;
+      context.strokeStyle="red";
+
+      var startX;
+      var startY;
+      var paint;
+      $canvas.mousedown(function(e){
+        var offset = $canvas.offset();
+        context.clearRect(0,0,width, height);
+        startX = e.pageX - offset.left;
+        startY = e.pageY - offset.top;
+        paint = true;
+        console.log(startX+" "+startY);
+        console.log(e.pageX+" "+e.pageY);
+        // context.strokeStyle = "red";
+        // context.strokeRect(x,y,10,10);
+        // context.stroke();
       });
-      $zoomout.on("click", function(){
-        zoom($img, $parent, originalHeight, 0.8);
+
+      $canvas.mousemove(function(e){
+        if(paint === true){
+          context.clearRect(0,0,width, height);
+          var offset = $canvas.offset();
+          x = e.pageX - offset.left;
+          y = e.pageY - offset.top;
+          var w = Math.abs(startX - x);
+          var h = Math.abs(startY - y);
+          context.strokeRect(Math.min(x,startX), Math.min(y,startY), w, h);
+        }
       });
+
+      $canvas.mouseup(function(){
+        paint = false;
+      });
+
+
     });
   };
-}( jQuery ));
+}(jQuery));
 
-var zoom = function($img, $parent, originalHeight, scale){
+
+var zoom = function($img, $parent, originalHeight, scale) {
   var offset = $img.offset();
   var width = $img.width();
   var height = $img.height();
@@ -54,7 +114,8 @@ var zoom = function($img, $parent, originalHeight, scale){
   var leftPercent = (($parent.width() / 2) - offset.left) / width;
   var topPercent = (($parent.height() / 2) - offset.top) / height;
 
-  $img.offset({top: offset.top - (diffHeight * topPercent), left: offset.left - (diffWidth * leftPercent)});
+  // $img.offset({top: offset.top - (diffHeight * topPercent), left: offset.left - (diffWidth * leftPercent)});
+  $img.offset({top: offset.top-20, left: offset.left - 20});
 
   $img.width(width * scale).height(height * scale);
 };
