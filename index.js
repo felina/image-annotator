f42b321b769b4812995a346b9bce5861fc1a0b8a
Alistair <alistair.wk@gmail.com>
2014-02-10 01:28:27 +0000

    attempt at OO

--- index.js
+++ index.js
@@ -13,18 +13,259 @@ var canvascss = {
   cursor : "move"
 };
 
+// Annotations - as distinct on the canvas
 function Annotation(type) {
   this.valid = false;
   this.pts = [{x:0,y:0}, {x:0,y:0}];
   this.type = type;
 };
 
-var att;
-var atts = new Array();
-var selectedType = "rect";
+
+//////////////////////////////////////////////////////
+///// Annotator Object Definition ////////////////////
+
+// Creates a new annotator (to be bound to a DOM object)
+function Annotator(src, w, h) {
+  // Parameters
+  this.src = src;
+  this.w = w;
+  this.h = h;
+
+  // Controls
+  this.zoomin = null;
+  this.zoomout = null;
+  this.pan = null;
+  this.annotate = null;
+  this.attType = null;
+
+  // Components
+  this.container = null
+  this.canvas = null;
+
+
+  // Annotations
+  this.att = null;
+  this.atts = new Array();
+  this.selectedType = "rect";
+
+  // Drawing
+  this.g = null;
+
+  // Transform info
+  this.curScale = 0.9;
+  this.xOffs = 0;
+  this.yOffs = 0;
+
+  // Canvas ops
+  this.x0 = 0;
+  this.x1 = 0;
+  this.y0 = 0;
+  this.y1 = 0;
+  this.curOp = "pan";
+  this.active = false;
+  this.polyC = 0;
+}
+
+Annotator.fn = Annotator.prototype;
+
+//////////////////////////////////////////////////////
+// Instantiates an annotator inside a DOM object
+Annotator.fn.build = function($parent) {
+  // Register and generate annotator components
+  $parent.addClass("annotator");
+  $parent.data("Annotator", this);
+
+  // Controls
+  this.zoomin    = $('<button id="zoomin">+</button>').appendTo($parent);
+  this.zoomout   = $('<button id="zoomout">-</button>').appendTo($parent);
+  this.pan       = $('<button id="pan">Pan</button>').appendTo($parent);
+  this.annotate  = $('<button id="annot">Annotate</button>').appendTo($parent);
+
+  this.attType      = $('<select id="typesel"></select>')
+                      .html('<option>Box</option><option>Polygon</option>')
+                      .appendTo($parent);
+
+  // Canvas container
+  this.container = $('<div></div>')
+                      .css(containercss)
+                      .width(this.w)
+                      .height(this.h)
+                      .appendTo($parent);
+
+  // Load the image
+  this.img = $('<img src="'+this.src+'"></img>')
+                      .width(this.w)
+                      .height(this.h)
+                      .hide()
+                      .appendTo($parent);
+
+  // The drawing canvas
+  this.canvas = $('<canvas>Unsupported browser.</canvas>')
+                      .css(canvascss)
+                      .appendTo(this.container);
+  // Resize canvas
+  this.canvas[0].width = this.w;
+  this.canvas[0].height = this.h;
+
+  // Get the drawing context
+  this.g = this.canvas[0].getContext("2d");
+
+  var a = this;
+
+  // Zoom control
+  this.zoomin.click(function(){a.zoom(1.25);});
+  this.zoomout.click(function(){a.zoom(0.8);});
+
+  // Operation selection
+  this.attType.change(function() {
+    var str = $(this).val();
+
+    if (str == "Box") {
+      a.selectedType = "rect";
+    }
+    else if (str == "Polygon") {
+      a.selectedType = "poly";
+    }
+  });
+
+  this.pan.click(function(){
+    a.curOp = "pan";
+    a.canvas.css("cursor", "move");
+  });
+
+  this.annotate.click(function(){
+    a.curOp = "annotate";
+    a.canvas.css("cursor", "crosshair");
+  });
+
+  // Mouse down - start drawing or panning
+  this.canvas.mousedown(function(e){
+    a.mbDown(e.pageX, e.pageY);
+  });
+
+  // Movement continues draw/pan as long as the mouse button is held
+  this.canvas.mousemove(function(e){
+    a.mMove(e.pageX, e.pageY);
+  });
+
+  // Operation end
+  this.canvas.mouseup(function(){
+    a.mbUp();
+  });
+
+  // We have to wait for the image to load before we can use it
+  this.img.load(function(){
+    a.doTransform();
+  });
+}
+
+//////////////////////////////////////////////////////
+// Mouse control
+Annotator.fn.mbDown = function(x, y) {
+  if (!this.active) {
+    var offset = this.canvas.offset();
+    this.x1 = this.x0 = x - offset.left;
+    this.y1 = this.y0 = y - offset.top;
+    this.active = true;
+
+    if (this.curOp == "annotate") {
+      this.att = new Annotation(this.selectedType);
+      this.att.valid = true;
+      this.atts.push(this.att);
+
+      if (this.att.type == "poly") {
+        this.att.pts[0] = new ptToImg(this.x0, this.y0);
+        this.polyC = 1;
+      }
+    }
+  }
+}
+
+Annotator.fn.mbUp = function() {
+  // End ONLY if dragged
+  if (this.curOp == "annotate") {
+    if (this.x0 != this.x1 && this.y0 != this.y1) {
+      if (this.att.type == "rect") this.active = false;
+      else if (this.att.type == "poly") {
+        this.x0 = this.x1;
+        this.y0 = this.y1;
+        this.polyC++;
+      }
+    }
+    else if (this.att.type == "poly" && this.polyC > 1) {
+      this.active = false;
+    }
+  }
+  else {
+    this.active = false;
+  }
+}
+
+Annotator.fn.mMove = function(x, y) {
+  if (!this.active) return;
+
+  var offset = this.canvas.offset();
+  this.x1 = x - offset.left;
+  this.y1 = y - offset.top;
+
+  var dx = this.x1 - this.x0;
+  var dy = this.y1 - this.y0;
+
+  if (this.curOp == "pan") {
+    // Panning the image
+    this.doPan(dx, dy);
+    this.x0 = this.x1;
+    this.y0 = this.y1;
+  }
+  else if (this.curOp == "annotate") {
+    // Annotation - in image space
+    var pt1 = new ptToImg(this, this.x0, this.y0);
+    var pt2 = new ptToImg(this, this.x1, this.y1);
+
+    if (this.att.type == "rect") {
+      this.att.pts[0] = pt1;
+      this.att.pts[1] = pt2;
+    }
+    else if (this.att.type == "poly") {
+      // Save next point
+      this.att.pts[this.polyC] = pt2;
+    }
+
+    // Redraw
+    this.doTransform();
+  }
+}
+
+// Updates an existing annotator with a new image
+// (Also resets the pan/zoom and annotations)
+Annotator.fn.update = function(src, w, h) {
+  this.src = src;
+  this.w = w;
+  this.h = h;
+
+  // Reloading & resizing
+  this.container.width(w).height(h);
+  this.img.attr("src", src).width(w).height(h);
+
+  // Reset pan/zoom
+  this.curScale = 0.9;
+  this.xOffs = 0;
+  this.yOffs = 0;
+
+  // Reset annotation
+  this.att = null;
+  this.atts = new Array();
+
+  // Resize canvas
+  this.canvas[0].width = w;
+  this.canvas[0].height = h;
+}
 
 // Canvas re-draw op
-function repaint(g, $img) {
+Annotator.fn.repaint = function() {
+  var g = this.g;
+  var $img = this.img;
+
   var w = $img.width();
   var h = $img.height();
 
@@ -36,19 +277,21 @@ function repaint(g, $img) {
   g.drawImage($img[0], -w/2, -h/2);
 
   // Annotation
-  for (var i = 0; i < atts.length; i++) {
-    drawAtt(g, atts[i]);
+  for (var i = 0; i < this.atts.length; i++) {
+    this.drawAtt(this.atts[i]);
   }
-};
+}
 
 // Annotation draw op
-function drawAtt(g, att) {
+Annotator.fn.drawAtt = function(att) {
+  var g = this.g;
+
   if (!att.valid) return;
 
   g.shadowColor = "#222";
   g.shadowBlur = 5;
   g.strokeStyle = "white";
-  g.lineWidth = 1 / curScale;
+  g.lineWidth = 1 / this.curScale;
   g.fillStyle = "white";
 
   // Box drawing (2-point)
@@ -65,10 +308,10 @@ function drawAtt(g, att) {
 
     g.strokeRect(x, y, dx, dy);
 
-    drawPt(g, {x:x0, y:y0});
-    drawPt(g, {x:x0, y:y1});
-    drawPt(g, {x:x1, y:y0});
-    drawPt(g, {x:x1, y:y1});
+    this.drawPt(g, {x:x0, y:y0});
+    this.drawPt(g, {x:x0, y:y1});
+    this.drawPt(g, {x:x1, y:y0});
+    this.drawPt(g, {x:x1, y:y1});
   }
   // Polygon drawing (n-point)
   else if (att.type == "poly") {
@@ -83,280 +326,92 @@ function drawAtt(g, att) {
     g.stroke();
 
     for (var i = 0; i < att.pts.length; i++) {
-      drawPt(g, att.pts[i]);
+      this.drawPt(att.pts[i]);
     }
   }
 }
 
 // Point drawing util
-function drawPt(g, pt) {
+Annotator.fn.drawPt = function(pt) {
+  var g = this.g;
   g.beginPath();
-  g.arc(pt.x, pt.y, 2.5/curScale, 0, 2*Math.PI, false);
+  g.arc(pt.x, pt.y, 2.5/this.curScale, 0, 2*Math.PI, false);
   g.fill();
 }
 
-// Transform info
-var curScale = 0.9;
-var xOffs = 0;
-var yOffs = 0;
-
 // General transform op
-function doTransform(g, $img) {
-  var w = $img.width();
-  var h = $img.height();
+Annotator.fn.doTransform = function() {
+  var g = this.g;
 
   // Reset xform & clear
   g.setTransform(1,0,0,1,0,0);
-  g.clearRect(0, 0, w, h);
+  g.clearRect(0, 0, this.w, this.h);
 
   // To draw in position with scaling,
   // move to position (translate), then
   // scale before drawing at (0, 0)
-  g.translate(w/2 + xOffs, h/2 + yOffs);
-  g.scale(curScale, curScale);
+  g.translate(this.w/2 + this.xOffs, this.h/2 + this.yOffs);
+  g.scale(this.curScale, this.curScale);
 
-  repaint(g, $img);
-};
-
-// Zoom op
-function zoom(g, $img, scale) {
-  // New scaling level
-  curScale *= scale;
-
-  if (curScale < 0.9) {
-    curScale = 0.9;
-  }
-
-  doTransform(g, $img);
-};
+  this.repaint();
+}
 
 // Pan op
-function pan(g, $img, x, y) {
+Annotator.fn.doPan = function(x, y) {
   // New offset
   var margin = 100;
 
-  xOffs += x;
-  yOffs += y;
+  this.xOffs += x;
+  this.yOffs += y;
 
-  var xLim = ($img.width()/2)*curScale;
-  var yLim = ($img.height()/2)*curScale;
+  var xLim = (this.w/2)*this.curScale;
+  var yLim = (this.h/2)*this.curScale;
 
-  if (xOffs >  xLim) xOffs =  xLim;
-  if (xOffs < -xLim) xOffs = -xLim;
-  if (yOffs >  yLim) yOffs =  yLim;
-  if (yOffs < -yLim) yOffs = -yLim;
+  if (this.xOffs >  xLim) this.xOffs =  xLim;
+  if (this.xOffs < -xLim) this.xOffs = -xLim;
+  if (this.yOffs >  yLim) this.yOffs =  yLim;
+  if (this.yOffs < -yLim) this.yOffs = -yLim;
 
-  doTransform(g, $img);
-};
+  this.doTransform();
+}
+
+// Zoom op
+Annotator.fn.zoom = function(scale) {
+  // New scaling level
+  this.curScale *= scale;
+  if (this.curScale < 0.9) this.curScale = 0.9;
+  this.doTransform();
+}
 
 // Util - canvas to image space
-function ptToImg($img, x, y) {
-  var out = {
-    x : 0, y : 0
-  }
+function ptToImg(a, x, y) {
+  var x = (x-a.w/2-a.xOffs)/a.curScale;
+  var y = (y-a.h/2-a.yOffs)/a.curScale;
 
-  out.x = (x-$img.width()/2-xOffs)/curScale;
-  out.y = (y-$img.height()/2-yOffs)/curScale;
+  return {x:x,y:y};
+}
 
-  return out;
-};
+
+//////////////////////////////////////////////////////
+///// API Definition /////////////////////////////////
 
 (function( $ ) {
   // The annotator function - appplicable to any jQuery object collection
   $.fn.annotator = function(src, width, height) {
     return this.each(function() {
-      var $zoomin, $zoomout, $pan, $annotate,
-          $container, $img, $canvas, $type, g;
-
       // Check for annotator class
       $parent = $(this);
+      var a;
 
       // Update if we're passed an existing annotator
       if ($parent.hasClass("annotator")) {
-        // Retrieve controls
-        $zoomin     = $parent.find("#zoomin");
-        $zoomout    = $parent.find("#zoomin");
-        $pan        = $parent.find("#pan");
-        $annotate   = $parent.find("#annot");
-
-        $type       = $parent.find("#typesel");
-
-        // Retrieve/resize container
-        $container = $parent.find("div").width(width).height(height);
-
-        // Reload the image
-        $img = $parent.find("img").attr("src", src).width(width).height(height);
-
-        // Retrieve canvas
-        $canvas = $parent.find("canvas");
-
-        // Reset pan/zoom
-        curScale = 0.9;
-        xOffs = 0;
-        yOffs = 0;
-
-        // Reset annotation
-        att = new Annotation(selectedType);
-        atts = [att];
+        a = $parent.data("Annotator");
+        a.update(src, width, height);
       }
       else {
-        // Register and generate annotator components
-        $parent.addClass("annotator");
-      
-        // Controls
-    	  $zoomin    = $('<button id="zoomin">+</button>').appendTo($parent);
-        $zoomout   = $('<button id="zoomout">-</button>').appendTo($parent);
-        $pan       = $('<button id="pan">Pan</button>').appendTo($parent);
-        $annotate  = $('<button id="annot">Annotate</button>').appendTo($parent);
-
-        $type      = $('<select id="typesel"></select>')
-                      .html('<option>Box</option><option>Polygon</option>')
-                      .appendTo($parent);
-
-        // Canvas container
-        $container = $('<div></div>')
-                    .css(containercss)
-                    .width(width)
-                    .height(width)
-                    .appendTo($parent);
-
-        // Load the image
-        $img = $('<img src="'+src+'"></img>')
-                    .width(width)
-                    .height(height)
-                    .hide()
-                    .appendTo($parent);
-
-        // The drawing canvas
-        $canvas = $('<canvas>Unsupported browser.</canvas>')
-                    .css(canvascss)
-                    .appendTo($container);
-
-        if (typeof G_vmlCanvasManager != 'undefined') {
-          console.log("um");
-          canvas = G_vmlCanvasManager.initElement(canvas);
-        }
-
-        // Zoom control
-        $zoomin.click(function(){zoom(g, $img, 1.25 );});
-        $zoomout.click(function(){zoom(g, $img, 0.8  );});
-
-        // Canvas operations
-        var x0, x1;
-        var y0, y1;
-        var op = "pan";
-        var active = false;
-        var polyC = 0;
-
-        // Operation selection
-        $type.change(function(){
-          var str = $(this).val();
-          if (str == "Box") {
-            selectedType = "rect";
-          }
-          else if (str == "Polygon") {
-            selectedType = "poly";
-          }
-        });
-        $pan.click(function(){
-          op = "pan";
-          $canvas.css("cursor", "move");
-        });
-        $annotate.click(function(){
-          op = "annotate";
-          $canvas.css("cursor", "crosshair");
-        });
-
-        // Mouse down - start drawing or panning
-        $canvas.mousedown(function(e){
-          if (!active) {
-            var offset = $canvas.offset();
-            x1 = x0 = e.pageX - offset.left;
-            y1 = y0 = e.pageY - offset.top;
-            active = true;
-
-            if (op == "annotate") {
-              att = new Annotation(selectedType);
-              att.valid = true;
-              atts.push(att);
-
-              if (att.type == "poly") {
-                att.pts[0] = ptToImg($img, x0, y0);
-                polyC = 1;
-              }
-            }
-          }
-        });
-
-        // Movement continues draw/pan as long as the mouse button is held
-        $canvas.mousemove(function(e){
-          if (!active) return;
-
-          var offset = $canvas.offset();
-          x1 = e.pageX - offset.left;
-          y1 = e.pageY - offset.top;
-
-          var dx = x1 - x0;
-          var dy = y1 - y0;
-
-          if (op == "pan") {
-            // Panning the image
-            pan(g, $img, dx, dy);
-            x0 = x1;
-            y0 = y1;
-          }
-          else if (op == "annotate") {
-            // Annotation - in image space
-            var pt1 = ptToImg($img, x0, y0);
-            var pt2 = ptToImg($img, x1, y1);
-
-            if (att.type == "rect") {
-              att.pts[0] = pt1;
-              att.pts[1] = pt2;
-            }
-            else if (att.type == "poly") {
-              // Save next point
-              att.pts[polyC] = pt2;
-            }
-
-            // Redraw
-            doTransform(g, $img);
-          }
-        });
-
-        // Operation end
-        $canvas.mouseup(function(){
-          // End ONLY if dragged
-          if (op == "annotate") {
-            if (x0 != x1 && y0 != y1) {
-              if (att.type == "rect") active = false;
-              else if (att.type == "poly") {
-                x0 = x1;
-                y0 = y1;
-                polyC++;
-              }
-            }
-            else if (att.type == "poly" && polyC > 1) {
-              active = false;
-            }
-          }
-          else {
-            active = false;
-          }
-        });
-      } // end of conditional update
-
-      // Scale the canvas to the original image
-      $canvas[0].width = width;
-      $canvas[0].height = height;
-
-      // 'g' is the graphics context for rendering
-      g = $canvas[0].getContext("2d");
-
-      // We have to wait for the image to load before we can use it
-      $img.load(function(){
-        doTransform(g, $img);
-      });
+        a = new Annotator(src, width, height);
+        a.build($parent);
+      }
     });
   };
 }(jQuery));
